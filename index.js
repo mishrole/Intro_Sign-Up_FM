b3ee7620cac0c45654820566e954eb9bfd0823c0
Mitchell Rodríguez <mitrodle@gmail.com>
2021-03-01 00:28:09 -0500

    Fix: Confirm message

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const intro = document.querySelector('.intro');
 const form = document.querySelector('.intro__signup--form');
 const inputs = document.querySelectorAll('.intro__signup--input');
 const btnClaim = document.querySelector('.intro__signup--cta');
@@ -15,14 +16,16 @@ function validateForm(e) {
     if(e.target.value.trimStart().length > 0) {
         e.target.classList.remove('border-red');
 
-        if(e.target.nextElementSibling.classList.value === 'error-message') {
+        if(e.target.nextElementSibling.classList.value === 'flex-container') {
             e.target.nextElementSibling.remove();
         }
     } else {
         e.target.classList.add('border-red');
         const message = notification(`${e.target.placeholder} cannot be empty`, 'error');
 
-        if(e.target.nextElementSibling.classList.value != 'error-message') {
+        console.error(e.target.nextElementSibling.classList.value)
+
+        if(e.target.nextElementSibling.classList.value !== 'flex-container') {
             form.insertBefore(message, e.target.nextElementSibling);
         }
     }
@@ -35,7 +38,7 @@ function validateForm(e) {
     if(e.target.type === 'email') {
         if(regexEmail.test(e.target.value) === false) {
 
-            if(e.target.nextElementSibling.classList.value != 'error-message') {
+            if(e.target.nextElementSibling.classList.value !== 'flex-container') {
                 const message = notification('Looks like this is not an email', 'error');
                 form.insertBefore(message, e.target.nextElementSibling);
                 e.target.classList.add('border-red');
@@ -46,15 +49,20 @@ function validateForm(e) {
 
 function claimTrial(e) {
     e.preventDefault();
+
     const message = notification('Free trial activated', 'success');
-    console.log(message)
+    intro.insertBefore(message, document.querySelector('.intro__presentation'));
 }
 
 function notification(message, type) {
+    const container = document.createElement('div');
+    container.classList.add(`flex-container`);
     const notification = document.createElement('p');
     notification.textContent = `${message}`;
     notification.classList.add(`${type}-message`);
-    return notification;
+    container.appendChild(notification);
+
+    return container;
 }
 
 
